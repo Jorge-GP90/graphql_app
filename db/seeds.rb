542ffed8fa6b2bf7d2e33eb77745053a42dca29c
George Garcia <jorge.90gap@gmail.com>
2021-11-30 22:28:49 +0900

    Faker gem and Data: seed were added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do
+  blog = Blog.create(title: Faker::JapaneseMedia::DragonBall.race, text: Faker::Quote.yoda)
+end
